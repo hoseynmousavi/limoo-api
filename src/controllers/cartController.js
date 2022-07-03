4aaf92cb66152894d9535134a77467a8e5970862
Hoseyn <hoseyn.mousavi78@gmail.com>
2022-07-03 22:36:00 +0430

    fx

--- src/controllers/cartController.js
+++ src/controllers/cartController.js
@@ -41,7 +41,7 @@ function getReviewCarts(req, res)
                             {
                                 pack_id: {$in: pack_id},
                                 $or: [
-                                    {index: 1, last_review_date: undefined, created_date: createBeforeDate({dayBefore: 2})},
+                                    {index: 1, last_review_date: undefined, created_date: {$gte: createBeforeDate({dayBefore: 2})}},
                                     {index: 1, last_review_date: {$gte: createBeforeDate({dayBefore: 2})}},
                                 ],
                             },
